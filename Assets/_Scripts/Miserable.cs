259b9fd43f334421edaaa1e0723c6c857b22c96c
CaldoMaggi <mquiroz984@soyudemedellin.edu.co>
2026-02-24 07:52:29 -0500

    Miserable get y set

--- Assets/_Scripts/Miserable.cs
+++ Assets/_Scripts/Miserable.cs
@@ -1,16 +1,28 @@
+using System;
 using UnityEngine;
 
-public class Miserable : MonoBehaviour
+public class Miserable : Player
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        Level = 1;
+        Nombre = "Miserable";
+        Vida = 414;
+        Concentracion = 78;
+        Estamina = 96;
+        PoderFisico = 40;
+        EquipLoad = "ligero";
+        Parry = false;
+    }
+    public void Atacar()
+    {
+        Debug.Log($"{Nombre} ataca con su garrote y hace {PoderFisico} puntos de daño");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ParryAble()
     {
-        
+        Parry = false;
+        Debug.Log($"{Nombre} no puede bloquear sin escudo");
     }
 }
+
