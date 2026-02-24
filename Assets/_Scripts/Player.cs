8e7ec40ea340c834910ccd5c4d62aabff7b4ae4b
CaldoMaggi <mquiroz984@soyudemedellin.edu.co>
2026-02-24 07:25:23 -0500

    Player script

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Atributos del jugador independiente de la clase escogida
+    private string nombre;
+    private int level;
+    private int vida;
+    private int concentracion; //mana
+    private int poderFisico;
+    private int poderMagico;
+    private int poderSanto;
+    private int estamina;
+    private string equipLoad; //ligero,mediano,pesado
+    private bool parry = false;
 }
+
