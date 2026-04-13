e13d1b6b86b62ed22357d680c404ae6a184d0a60
CaldoMaggi <mquiroz984@soyudemedellin.edu.co>
2026-04-13 15:27:41 -0500

    Camara y figuras

--- Assets/_Scripts/Camara.cs
+++ Assets/_Scripts/Camara.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
 
-public class SeguirJugador : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    public KeyCode switchKey;
+    public Transform objetivo;
+    public Vector3 offset = new Vector3(0, 1, -1);
 
-    public GameObject jugador;
-    private Vector3 offset = new Vector3(0, 6, -10);
-
-    private void FixedUpdate()
+    void LateUpdate()
     {
-        transform.position = jugador.transform.position + offset;
+        // 1. Buscar al jugador si se perdió
+        if (objetivo == null)
+        {
+            GameObject found = GameObject.FindWithTag("Player");
+            if (found != null) objetivo = found.transform;
+            return;
+        }
+
+        // 2. MOVER LA CÁMARA (Esto es lo que te faltaba)
+        // La cámara se posiciona exactamente a la distancia del offset
+        transform.position = objetivo.position + offset;
+
+        // 3. ROTAR LA CÁMARA
+        // Ahora que ya está en posición, mira al objetivo
+        transform.LookAt(objetivo.position);
     }
 }
